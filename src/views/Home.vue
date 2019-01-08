95251692dbfaced6cc4e81f039aeac95fa393184
Piotr Schaar <piotr.schaar@gmail.com>
2019-01-08 22:29:17 +0100

    Revert "component favorite list"

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,10 @@
 <template>
   <div>
-    <transition name="slideTop">
+    <transition name="slide">
       <SearchBar v-if="searchBar==true" @clicked="addContact"></SearchBar>
     </transition>
-    <transition name="slideRight">
-      <Favorite :favoriteContacts="favoriteContacts" v-if="FavoritesWrapper == true" v-on:toggleFavoritesWrapper="FavoritesWrapper = !FavoritesWrapper"></Favorite>
-    </transition>
-
     <div class="wrapper light">
-      <Nav
-        v-on:toggleSearchBar="searchBar = !searchBar"
-        v-on:toggleFavoritesWrapper="FavoritesWrapper = !FavoritesWrapper"
-        :pageTitle="pageTitle"
-      ></Nav>
+      <Nav v-on:toggleSearchBar="searchBar = !searchBar" :pageTitle="pageTitle"></Nav>
       <Contacts
         :contacts="contacts"
         @deleted="deleteContact"
@@ -28,21 +20,18 @@ import axios from "axios";
 import Nav from "@/components/Nav.vue";
 import Contacts from "@/components/Contacts.vue";
 import SearchBar from "@/components/SearchBar.vue";
-import Favorite from "@/components/Favorite.vue";
 export default {
   name: "Home",
   components: {
     Contacts,
     Nav,
-    SearchBar,
-    Favorite
+    SearchBar
   },
   data() {
     return {
       pageTitle: "Connector",
       contacts: [],
       searchBar: false,
-      FavoritesWrapper: false,
       isLoading: false,
       error: null,
       favoriteContacts: []
@@ -64,55 +53,32 @@ export default {
     addToFavorited(value) {
       value.isFavorite = true;
       this.favoriteContacts.push(value);
-      this.saveContacts();
-      this.saveFavoriteContacts();
     },
     unFavorited(value) {
       value.isFavorite = false;
       this.favoriteContacts.splice(this.contacts.indexOf(value), 1);
-      this.saveContacts();
-      this.saveFavoriteContacts();
     },
     saveContacts() {
       let parsed = JSON.stringify(this.contacts);
       localStorage.setItem("contacts", parsed);
     },
-    saveFavoriteContacts() {
-      let parsed = JSON.stringify(this.favoriteContacts);
-      localStorage.setItem("favoriteContacts", parsed);
-    }
   },
 
   mounted() {
     if (localStorage.getItem("contacts")) {
       this.contacts = JSON.parse(localStorage.getItem("contacts"));
     }
-    if (localStorage.getItem("favoriteContacts")) {
-      this.favoriteContacts = JSON.parse(
-        localStorage.getItem("favoriteContacts")
-      );
-    }
   }
 };
 </script>
 
 <style>
-.slideTop-enter-active,
-.slideTop-leave-active {
+.slide-enter-active,
+.slide-leave-active {
   transition: margin-top 0.5s ease;
 }
-.slideTop-enter,
-.slideTop-leave-to {
+.slide-enter,
+.slide-leave-to {
   margin-top: -400px;
-}
-.slideRight-enter-active,
-.slideRight-leave-active {
-  transition: all 0.3s ease;
-}
-
-.slideRight-enter,
-.slideRight-leave-to {
-  transform: translateX(100vh);
-  opacity: 0;
 }
 </style>
